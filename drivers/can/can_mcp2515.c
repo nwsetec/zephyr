fd52a5b19eed4aa426bbf707b06aa0efc7dfb1b6
Nick Ward <nick.ward@setec.com.au>
2019-08-08 00:23:34 +1000

    drivers: mcp2515: Add and use RTS command
    Reduces SPI data required for request to send from 4 bytes to 1

--- drivers/can/can_mcp2515.c
+++ drivers/can/can_mcp2515.c
@@ -78,6 +78,20 @@ static int mcp2515_cmd_load_tx_buffer(struct device *dev, u8_t tx_idx,
 	return spi_write(DEV_DATA(dev)->spi, &DEV_DATA(dev)->spi_cfg, &tx);
 }
 
+static int mcp2515_cmd_rts(struct device *dev, u8_t tx_idx)
+{
+	u8_t cmd_buf[] = { MCP2515_OPCODE_RTS | BIT(tx_idx) };
+
+	struct spi_buf tx_buf[] = {
+		{ .buf = cmd_buf, .len = sizeof(cmd_buf) }
+	};
+	const struct spi_buf_set tx = {
+		.buffers = tx_buf, .count = ARRAY_SIZE(tx_buf)
+	};
+
+	return spi_write(DEV_DATA(dev)->spi, &DEV_DATA(dev)->spi_cfg, &tx);
+}
+
 static int mcp2515_cmd_read_reg(struct device *dev, u8_t reg_addr,
 				u8_t *buf_data, u8_t buf_len)
 {
@@ -342,7 +356,6 @@ static int mcp2515_send(struct device *dev, const struct zcan_frame *msg,
 {
 	struct mcp2515_data *dev_data = DEV_DATA(dev);
 	u8_t tx_idx = 0U;
-	u8_t addr_tx_ctrl;
 	u8_t tx_frame[MCP2515_FRAME_LEN];
 
 	if (k_sem_take(&dev_data->tx_sem, timeout) != 0) {
@@ -369,15 +382,11 @@ static int mcp2515_send(struct device *dev, const struct zcan_frame *msg,
 	dev_data->tx_cb[tx_idx].cb = callback;
 	dev_data->tx_cb[tx_idx].cb_arg = callback_arg;
 
-	addr_tx_ctrl = MCP2515_ADDR_TXB0CTRL +
-		       (tx_idx * MCP2515_ADDR_OFFSET_FRAME2FRAME);
-
 	mcp2515_convert_zcanframe_to_mcp2515frame(msg, tx_frame);
 	mcp2515_cmd_load_tx_buffer(dev, tx_idx, tx_frame);
 
 	/* request tx slot transmission */
-	mcp2515_cmd_bit_modify(dev, addr_tx_ctrl, MCP2515_TXCTRL_TXREQ,
-			       MCP2515_TXCTRL_TXREQ);
+	mcp2515_cmd_rts(dev, tx_idx);
 
 	if (callback == NULL) {
 		k_sem_take(&dev_data->tx_cb[tx_idx].sem, K_FOREVER);
